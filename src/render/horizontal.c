8870c6021399ba5ad3bb637f70fa5c5a5495fba4
Nordup <nordup.ondr@gmail.com>
2020-09-07 20:28:51 +0300

    norm ray_casting.c

--- src/render/horizontal.c
+++ src/render/horizontal.c
@@ -1,5 +1,40 @@
 #include "render.h"
 
+void		define_horizontal_fields1(t_pnt *A, t_pnt *dlta, int *cardinal_point, t_pnt pnt, float alp)
+{
+	if (alp < PI)
+	{
+		A->y = (int)pnt.y + 1;
+		dlta->y = 1;
+		A->x = pnt.x + (A->y - pnt.y) / tan(alp);
+		dlta->x = 1 / tan(alp);
+		*cardinal_point = North;
+	}
+	else
+	{
+		A->y = (int)pnt.y - EPS;
+		dlta->y = -1;
+		A->x = pnt.x + (A->y - pnt.y) / tan(alp);
+		dlta->x = -1 / tan(alp);
+		*cardinal_point = South;
+	}
+}
+
+
+void		define_horizontal_fields2(t_pnt *A, t_map *map, t_pnt pnt, float alp)
+{
+	if (alp == PI)
+	{
+		A->y = pnt.y;
+		A->x = -1;
+	}
+	else if (alp == 0)
+	{
+		A->y = pnt.y;
+		A->x = map->w;
+	}
+}
+
 t_rcasting	horizontal(t_map *map, t_pnt pnt, float alp)
 {
 	t_pnt	A;
@@ -8,32 +43,8 @@ t_rcasting	horizontal(t_map *map, t_pnt pnt, float alp)
 	int		type;
 
 	type = 0;
-	if (alp < PI)
-	{
-		A.y = (int)pnt.y + 1;
-		dlta.y = 1;
-		A.x = pnt.x + (A.y - pnt.y) / tan(alp);
-		dlta.x = 1 / tan(alp);
-		cardinal_point = North;
-	}
-	else
-	{
-		A.y = (int)pnt.y - EPS;
-		dlta.y = -1;
-		A.x = pnt.x + (A.y - pnt.y) / tan(alp);
-		dlta.x = -1 / tan(alp);
-		cardinal_point = South;
-	}
-	if (alp == PI)
-	{
-		A.y = pnt.y;
-		A.x = -1;
-	}
-	else if (alp == 0)
-	{
-		A.y = pnt.y;
-		A.x = map->w;
-	}
+	define_horizontal_fields1(&A, &dlta, &cardinal_point, pnt, alp);
+	define_horizontal_fields2(&A, map, pnt, alp);
 	while (A.y >= 0 && A.y < map->h)
 	{
 		if (A.x >= 0 && A.x < map->w)
--- src/render/vertical.c
+++ src/render/vertical.c
@@ -1,5 +1,40 @@
 #include "render.h"
 
+void		define_vertical_fields1(t_pnt *A, t_pnt *dlta, int *cardinal_point, t_pnt pnt, float alp)
+{
+	if (alp < PI_2 || alp > PI3_2)
+	{
+		A->x = (int)pnt.x + 1;
+		dlta->x = 1;
+		A->y = pnt.y + (A->x - pnt.x) * tan(alp);
+		dlta->y = tan(alp);
+		*cardinal_point = West;
+	}
+	else
+	{
+		A->x = (int)pnt.x - EPS;
+		dlta->x = -1;
+		A->y = pnt.y + (A->x - pnt.x) * tan(alp);
+		dlta->y = -tan(alp);
+		*cardinal_point = East;
+	}
+}
+
+void		define_vertical_fields2(t_pnt *A, t_map *map, t_pnt pnt, float alp)
+{
+	if (alp == PI_2)
+	{
+		A->y = map->h;
+		A->x = pnt.x;
+	}
+	else if (alp == PI3_2)
+	{
+		A->y = -1;
+		A->x = pnt.x;
+	}
+
+}
+
 t_rcasting	vertical(t_map *map, t_pnt pnt, float alp)
 {
 	t_pnt	A;
@@ -8,32 +43,8 @@ t_rcasting	vertical(t_map *map, t_pnt pnt, float alp)
 	int		type;
 
 	type = 0;
-	if (alp < PI_2 || alp > PI3_2)
-	{
-		A.x = (int)pnt.x + 1;
-		dlta.x = 1;
-		A.y = pnt.y + (A.x - pnt.x) * tan(alp);
-		dlta.y = tan(alp);
-		cardinal_point = West;
-	}
-	else
-	{
-		A.x = (int)pnt.x - EPS;
-		dlta.x = -1;
-		A.y = pnt.y + (A.x - pnt.x) * tan(alp);
-		dlta.y = -tan(alp);
-		cardinal_point = East;
-	}
-	if (alp == PI_2)
-	{
-		A.y = map->h;
-		A.x = pnt.x;
-	}
-	else if (alp == PI3_2)
-	{
-		A.y = -1;
-		A.x = pnt.x;
-	}
+	define_vertical_fields1(&A, &dlta, &cardinal_point, pnt, alp);
+	define_vertical_fields2(&A, map, pnt, alp);
 	while (A.x >= 0 && A.x < map->w)
 	{
 		if (A.y >= 0 && A.y < map->h)
